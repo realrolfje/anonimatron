aa4e10be33ed27cae6f1eb7105a9c31dcbf7be92
Rolf <no-reply@example.com>
2018-08-08 21:44:22 +0200

    Added old test.

---
+++ src/test/java/com/rolfje/anonimatron/file/CsvFileReaderTest.java
@@ -0,0 +1,24 @@
+package com.rolfje.anonimatron.file;
+
+import junit.framework.TestCase;
+
+import java.io.*;
+
+public class CsvFileReaderTest extends TestCase {
+
+	public void testHappy() throws IOException {
+
+		File tempFile = File.createTempFile(CsvFileReaderTest.class.getSimpleName(), ".csv");
+		Writer writer = new BufferedWriter(new FileWriter(tempFile));
+		writer.write("\"Testfield1\";\"Testfield2\"");
+		writer.close();
+
+
+		CsvFileReader csvFileReader = new CsvFileReader(tempFile);
+		assertTrue(csvFileReader.hasRecords());
+		Record read = csvFileReader.read();
+
+		assertEquals("Testfield1", read.getValues()[0]);
+		assertEquals("Testfield2", read.getValues()[1]);
+	}
+}
