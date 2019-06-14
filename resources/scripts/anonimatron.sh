b316510219c08a9baf3e74f6179ff6b524029aeb
Rolf <>
2019-06-14 17:42:03 +0200

    Fix for #44

--- resources/scripts/anonimatron.sh
+++ resources/scripts/anonimatron.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd $(realpath $(dirname $0))
+cd -P "$(dirname $0)"
 java -Xmx2G -classpath *:./libraries/*:./jdbcdrivers/*:./anonymizers/* com.rolfje.anonimatron.Anonimatron $*
 cd -
