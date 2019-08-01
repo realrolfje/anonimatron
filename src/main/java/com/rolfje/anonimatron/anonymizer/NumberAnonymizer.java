60866b484ba758490d99fd089c0aa0bc43d80d4d
Rolf <>
2019-08-01 22:05:29 +0200

    Separate class for generating BigDecimal random numbers.

--- src/main/java/com/rolfje/anonimatron/anonymizer/NumberAnonymizer.java
+++ src/main/java/com/rolfje/anonimatron/anonymizer/NumberAnonymizer.java
@@ -2,6 +2,7 @@ package com.rolfje.anonimatron.anonymizer;
 
 import com.rolfje.anonimatron.synonyms.NumberSynonym;
 import com.rolfje.anonimatron.synonyms.Synonym;
+import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -39,11 +40,40 @@ public class NumberAnonymizer implements Anonymizer {
         } else if (from instanceof Short) {
             return Short.valueOf((short) r.nextInt(Short.MAX_VALUE));
         } else if (from instanceof BigDecimal) {
-            return new BigDecimal(r.nextGaussian());
+            return new RandomBigDecimal().next();
         } else if (from instanceof BigInteger) {
             return BigInteger.valueOf(r.nextLong());
         } else {
             throw new IllegalArgumentException(from.getClass().getSimpleName() + " is not supported for " + this.getType());
+        }
+    }
+
+    private class RandomBigDecimal {
+        BigDecimal max = BigDecimal.valueOf(Double.MAX_VALUE);
+        BigDecimal min = BigDecimal.valueOf(Double.MIN_VALUE);
+
+        public RandomBigDecimal() {
+        }
+
+        public RandomBigDecimal(String min, String max) {
+            if (StringUtils.isNotEmpty(min)) {
+                this.min = new BigDecimal(min);
+            }
+
+            if (StringUtils.isNotEmpty(max)) {
+                this.max = new BigDecimal(max);
+            }
+        }
+
+        BigDecimal next() {
+            // TODO imperfect; does not take into account decimal places.
+            // default implementation could take decimal places from the original,
+            // so that amounts stored as floats (which is a TERRIBLE idea)
+            // result in new amounts with the same number of decimal places.
+
+            BigDecimal high = max.multiply(BigDecimal.valueOf(r.nextDouble()));
+            BigDecimal low = min.multiply(BigDecimal.valueOf(r.nextDouble()));
+            return high.add(low);
         }
     }
 }
