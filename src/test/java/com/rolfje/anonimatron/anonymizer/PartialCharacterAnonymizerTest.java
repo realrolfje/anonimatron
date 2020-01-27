3a4c1c3504c39fad5f12518bf26293a558dd678f
Bartłomiej Komendarczuk <bkomendarczuk@gmail.com>
2020-01-27 13:56:02 +0100

    Added additional tests.

--- src/test/java/com/rolfje/anonimatron/anonymizer/PartialCharacterAnonymizerTest.java
+++ src/test/java/com/rolfje/anonimatron/anonymizer/PartialCharacterAnonymizerTest.java
@@ -38,6 +38,21 @@ public class PartialCharacterAnonymizerTest extends TestCase {
         Assert.assertNotSame(to.substring(5) +" is not different than " + from.substring(5), to.substring(5) ,from.substring(5));
     }
 
+    public void testShouldNotAnonymizeTheFirstCharacters(){
+
+        String from = "TEST_ANONYMIZER";
+        String CHARS = "ABCDEFGHIJKLMNOPRSTUWVXYZ";
+        partialCharacterAnonymizer.NUMBER_OF_SKIPPED_CHARACTERS = 5;
+
+        Map<String, String> m = new HashMap<>();
+        m.put(PartialCharacterAnonymizer.PARAMETER, CHARS);
+        Synonym synonym = partialCharacterAnonymizer.anonymize(from, 5, false, m);
+
+        String to = (String) synonym.getTo();
+
+        Assert.assertEquals(to.substring(0,5) + " is not same as " + from.substring(0,5),to.substring(0,5), from.substring(0,5));
+    }
+
     public void testShouldAnonymizeCharactersAfterSkippingCharactersUsingDigitals(){
 
         String from = "3551085";
@@ -51,5 +66,20 @@ public class PartialCharacterAnonymizerTest extends TestCase {
         String to = (String) synonym.getTo();
 
         Assert.assertNotSame(to.substring(3) +" is not different than " + from.substring(3), to.substring(3) ,from.substring(3));
+    }
+
+    public void testShouldNotAnonymizeTheFirstCharactersUsingDigitals(){
+
+        String from = "3551085";
+        String CHARS = "1234567890";
+        partialCharacterAnonymizer.NUMBER_OF_SKIPPED_CHARACTERS = 3;
+
+        Map<String, String> m = new HashMap<>();
+        m.put(PartialCharacterAnonymizer.PARAMETER, CHARS);
+        Synonym synonym = partialCharacterAnonymizer.anonymize(from, 5, false, m);
+
+        String to = (String) synonym.getTo();
+
+        Assert.assertEquals(to.substring(0,3) + " is not same as " + from.substring(0,3),to.substring(0,3), from.substring(0,3));
     }
 }
