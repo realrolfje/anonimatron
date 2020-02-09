f23ad1ceb25bcf07434c16d5cb5b806654ec3208
Rolf <>
2020-02-09 19:59:36 +0100

    Changed behavior so that this returns only digits, and the skip parameter tells how many digits to skip from the start.

---
+++ src/main/java/com/rolfje/anonimatron/anonymizer/PartialDigitsAnonymizer.java
@@ -0,0 +1,64 @@
+package com.rolfje.anonimatron.anonymizer;
+
+import com.rolfje.anonimatron.synonyms.StringSynonym;
+import com.rolfje.anonimatron.synonyms.Synonym;
+
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Generates an output string based on the configured characters
+ * omitting the specified number of initial characters.
+ *
+ * @author Bartłomiej Komendarczuk
+ */
+
+public class PartialDigitsAnonymizer implements Anonymizer {
+    private static final Random random = new Random();
+
+    private static final String DIGITS = "0123456789";
+
+    public static String PARAMETER = "skip";
+
+    public PartialDigitsAnonymizer() {
+    }
+
+    @Override
+    public String getType() {
+        return "PARTIALRANDOMDIGITS";
+    }
+
+    @Override
+    public Synonym anonymize(Object from, int size, boolean shortlived, Map<String, String> parameters) {
+        if (parameters == null || !parameters.containsKey(PARAMETER)) {
+            throw new UnsupportedOperationException("Please provide '" + PARAMETER + "' parameter to indicate the number of characters to skip.");
+        }
+        return anonymize(from, size, shortlived, Integer.valueOf(parameters.get(PARAMETER)));
+    }
+
+    @Override
+    public Synonym anonymize(Object from, int size, boolean shortlived) {
+        return anonymize(from, size, shortlived, 0);
+    }
+
+    private Synonym anonymize(Object from, int size, boolean shortlived, int charactersToSkip) {
+        String fromString = from.toString();
+        int length = fromString.length();
+
+        StringBuilder sb = new StringBuilder(fromString.substring(0, charactersToSkip));
+        for (int i = charactersToSkip; i < length; i++) {
+            sb.append(sb.append(DIGITS.charAt(random.nextInt(DIGITS.length()))));
+        }
+
+        String to = sb.toString();
+
+        StringSynonym stringSynonym = new StringSynonym(
+                getType(),
+                fromString,
+                to,
+                shortlived
+        );
+
+        return stringSynonym;
+    }
+}
--- src/test/java/com/rolfje/anonimatron/anonymizer/PartialCharacterAnonymizerTest.java
+++ src/test/java/com/rolfje/anonimatron/anonymizer/PartialCharacterAnonymizerTest.java
@@ -8,44 +8,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Tests for {@link PartialCharacterAnonymizer}.
+ * Tests for {@link PartialDigitsAnonymizer}.
  *
  * @author Bartłomiej Komendarczuk
  */
 
 public class PartialCharacterAnonymizerTest extends TestCase {
 
-    private PartialCharacterAnonymizer partialCharacterAnonymizer = new PartialCharacterAnonymizer();
+    private PartialDigitsAnonymizer partialCharacterAnonymizer = new PartialDigitsAnonymizer();
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        partialCharacterAnonymizer = new PartialCharacterAnonymizer();
+        partialCharacterAnonymizer = new PartialDigitsAnonymizer();
     }
 
     public void testShouldAnonymizeCharactersAfterSkippingCharacters(){
 
-        String from = "TEST_ANONYMIZER";
-        String CHARS = "ABCDEFGHIJKLMNOPRSTUWVXYZ";
-        partialCharacterAnonymizer.NUMBER_OF_SKIPPED_CHARACTERS = 5;
+        String from = "TEST_123";
 
         Map<String, String> m = new HashMap<>();
-        m.put(PartialCharacterAnonymizer.PARAMETER, CHARS);
+        m.put(PartialDigitsAnonymizer.PARAMETER, String.valueOf(3));
         Synonym synonym = partialCharacterAnonymizer.anonymize(from, 5, false, m);
 
         String to = (String) synonym.getTo();
 
-        Assert.assertNotSame(to.substring(5) +" is not different than " + from.substring(5), to.substring(5) ,from.substring(5));
+        Assert.assertNotSame(to.substring(3) +" is not different than " + from.substring(3), to.substring(3) ,from.substring(3));
     }
 
     public void testShouldNotAnonymizeTheFirstCharacters(){
 
         String from = "TEST_ANONYMIZER";
-        String CHARS = "ABCDEFGHIJKLMNOPRSTUWVXYZ";
-        partialCharacterAnonymizer.NUMBER_OF_SKIPPED_CHARACTERS = 5;
 
         Map<String, String> m = new HashMap<>();
-        m.put(PartialCharacterAnonymizer.PARAMETER, CHARS);
+        m.put(PartialDigitsAnonymizer.PARAMETER, String.valueOf(5));
         Synonym synonym = partialCharacterAnonymizer.anonymize(from, 5, false, m);
 
         String to = (String) synonym.getTo();
@@ -56,12 +52,10 @@ public class PartialCharacterAnonymizerTest extends TestCase {
     public void testShouldAnonymizeCharactersAfterSkippingCharactersUsingDigitals(){
 
         String from = "3551085";
-        String CHARS = "1234567890";
-        partialCharacterAnonymizer.NUMBER_OF_SKIPPED_CHARACTERS = 3;
 
         Map<String, String> m = new HashMap<>();
-        m.put(PartialCharacterAnonymizer.PARAMETER, CHARS);
-        Synonym synonym = partialCharacterAnonymizer.anonymize(from, 5, false, m);
+        m.put(PartialDigitsAnonymizer.PARAMETER, String.valueOf(3));
+        Synonym synonym = partialCharacterAnonymizer.anonymize(from, 3, false, m);
 
         String to = (String) synonym.getTo();
 
@@ -71,15 +65,13 @@ public class PartialCharacterAnonymizerTest extends TestCase {
     public void testShouldNotAnonymizeTheFirstCharactersUsingDigitals(){
 
         String from = "3551085";
-        String CHARS = "1234567890";
-        partialCharacterAnonymizer.NUMBER_OF_SKIPPED_CHARACTERS = 3;
 
         Map<String, String> m = new HashMap<>();
-        m.put(PartialCharacterAnonymizer.PARAMETER, CHARS);
+        m.put(PartialDigitsAnonymizer.PARAMETER, String.valueOf(5));
         Synonym synonym = partialCharacterAnonymizer.anonymize(from, 5, false, m);
 
         String to = (String) synonym.getTo();
 
-        Assert.assertEquals(to.substring(0,3) + " is not same as " + from.substring(0,3),to.substring(0,3), from.substring(0,3));
+        Assert.assertEquals(to.substring(0,5) + " is not same as " + from.substring(0,5),to.substring(0,5), from.substring(0,5));
     }
 }
