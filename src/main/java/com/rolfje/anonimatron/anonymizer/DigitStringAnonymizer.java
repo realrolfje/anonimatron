e4e00c4dbc6eac7132c4cc50c5cc5d620d719a70
Rolf <>
2020-02-10 23:28:10 +0100

    Added digit masking capability to DigitStringAnonymizer

--- src/main/java/com/rolfje/anonimatron/anonymizer/DigitStringAnonymizer.java
+++ src/main/java/com/rolfje/anonimatron/anonymizer/DigitStringAnonymizer.java
@@ -3,11 +3,23 @@ package com.rolfje.anonimatron.anonymizer;
 import com.rolfje.anonimatron.synonyms.StringSynonym;
 import com.rolfje.anonimatron.synonyms.Synonym;
 
+import java.util.Map;
+
 public class DigitStringAnonymizer extends AbstractElevenProofAnonymizer {
+
+	final static String PARAMETER = "mask";
 
 	@Override
 	public String getType() {
 		return "RANDOMDIGITS";
+	}
+
+	@Override
+	public Synonym anonymize(Object from, int size, boolean shortlived, Map<String, String> parameters) {
+		if (parameters == null || !parameters.containsKey(PARAMETER)) {
+			throw new UnsupportedOperationException("Please provide '" + PARAMETER + "' with a digit mask in the form 111******, where only stars are replaced with random characters.");
+		}
+		return anonymizeMasked(from, size, shortlived, parameters.get(PARAMETER));
 	}
 
 	@Override
@@ -27,4 +39,48 @@ public class DigitStringAnonymizer extends AbstractElevenProofAnonymizer {
 		return stringSynonym;
 	}
 
+	/**
+	 * Anonymizes a string with digits into a new string of digits where only a part
+	 * of the original digits are changed. You can use this to mask out a certain part
+	 * of the original string, like an area code of a phone number, or the bank number
+	 * of a credit card number.
+	 *
+	 * A mask looks like "111***", where digits remain original, and stars (or other
+	 * characters) are replaced with random digits. An example:
+	 *
+	 * Original number: 555-1234
+	 * Mask:            1111**** (note that the '-' is also untouched by placing a digit there)
+	 * New number     : 555-9876
+	 *
+	 * Note that if the mask is shorter than the input string, the output string will
+	 * have random digits where there is no mask. In short: only the locations where
+	 * there is a digit in the mask are left untouched and will be in the output.
+	 *
+	 * @param from
+	 * @param size
+	 * @param shortlived
+	 * @param mask
+	 * @return
+	 */
+	private Synonym anonymizeMasked(Object from, int size, boolean shortlived, String mask) {
+		Synonym anonymize = anonymize(from, size, shortlived);
+
+		char[] fromChars = anonymize.getFrom().toString().toCharArray();
+		char[] toChars = anonymize.getTo().toString().toCharArray();
+
+		int length = Math.min(Math.min(mask.length(), fromChars.length), toChars.length);
+		for (int i = 0; i < length; i++) {
+			if (Character.isDigit(mask.charAt(i))) {
+				// Not masked, put original digit in output
+				toChars[i] = fromChars[i];
+			}
+		}
+
+		return new StringSynonym(
+				getType(),
+				String.valueOf(fromChars),
+				String.valueOf(toChars),
+				shortlived
+		);
+	}
 }
--- src/test/java/com/rolfje/anonimatron/anonymizer/DigitStringAnonymizerTest.java
+++ src/test/java/com/rolfje/anonimatron/anonymizer/DigitStringAnonymizerTest.java
@@ -1,15 +1,25 @@
 package com.rolfje.anonimatron.anonymizer;
 
 import com.rolfje.anonimatron.synonyms.Synonym;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
 public class DigitStringAnonymizerTest {
 
+    private DigitStringAnonymizer anonymizer;
+
+    @Before
+    public void setUp() throws Exception {
+        anonymizer = new DigitStringAnonymizer();
+    }
+
     @Test
     public void anonymize() {
-        DigitStringAnonymizer anonymizer = new DigitStringAnonymizer();
         String original = "ORIGINAL";
 
         Synonym synonym = anonymizer.anonymize(original, Integer.MAX_VALUE, false);
@@ -19,5 +29,37 @@ public class DigitStringAnonymizerTest {
         assertEquals(anonymizer.getType(), synonym.getType());
         assertEquals(original.length(), synonym.getTo().toString().length());
         assertFalse(synonym.isShortLived());
+    }
+
+    @Test
+    public void testMaskedAnonymize() {
+        String original = "ABCDEFGH";
+        String mask     = "1,1-1*99";
+        String expected = "AxCxExGH";
+
+        Synonym synonym = anonymizer.anonymize(
+                original, Integer.MAX_VALUE, false,
+                getMaskParameters(mask)
+        );
+
+        String toString = synonym.getTo().toString();
+
+        assertEquals(original, synonym.getFrom());
+        assertNotEquals(synonym.getFrom(), synonym.getTo());
+        assertEquals(synonym.getFrom().toString().length(), toString.length());
+
+        char[] toChars = toString.toCharArray();
+        char[] expectedChars = expected.toCharArray();
+
+        for (int i = 0; i < expectedChars.length; i++) {
+            assertTrue("Character at position " + i + " not what we expected. String is '" + toString + "'",
+                    (expectedChars[i] == 'x') == (Character.isDigit(toChars[i])));
+        }
+    }
+
+    private Map<String, String> getMaskParameters(String mask) {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put(anonymizer.PARAMETER, mask);
+        return parameters;
     }
 }
