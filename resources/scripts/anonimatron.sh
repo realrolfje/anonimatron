068300f419c89b1916f9ac656c366f0a2dd40f8b
Rolf <noreply@example.com>
2023-11-05 11:59:37 +0100

    Fixed java opts for the correct heap memory setting

--- resources/scripts/anonimatron.sh
+++ resources/scripts/anonimatron.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
 cd -P "$(dirname $0)"
-java ${JAVA_OPTS:='-Xmx2G'} -classpath *:./libraries/*:./jdbcdrivers/*:./anonymizers/* com.rolfje.anonimatron.Anonimatron $*
+java ${JAVA_OPTS:='-Xmx2G'} \
+  -classpath *:./libraries/*:./jdbcdrivers/*:./anonymizers/* \
+  --add-opens java.xml/com.sun.org.apache.xml.internal.serialize=ALL-UNNAMED \
+  com.rolfje.anonimatron.Anonimatron $*
 cd -
